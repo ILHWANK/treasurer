d19416b9805326449df26d3a772773fa6295c80a
ILHWANK <50158086+ILHWANK@users.noreply.github.com>
2021-11-28 16:03:48 +0900

    Treasurer_1.0.9
    카메라 시점 변경 기능 추가
    1. 컴퓨터 -> left shift
    2. 모바일 우측 숫자 버튼
    두환경 전부 시점이 변경되면 버튼의 숫자가 해당 카메라의 숫자를 나타냄

--- Assets/CameraCh.cs
+++ Assets/CameraCh.cs
@@ -1,12 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraCh : MonoBehaviour
 {
-    public GameObject camera;
-    void Start()
+    public Camera[] cameraItem;//카메라 활성 비활성화 
+
+    int cameraCount = 3;//카메라 갯수
+    int nowCamera = 0;// 활성화된 카메라 번호
+
+    public Text cameraText;
+
+    public void CameraChange()
     {
-        Transform cameratr = GetComponent<Transform>();
+        ++nowCamera;
+
+        if (nowCamera >= cameraCount)
+        {
+            nowCamera = 0;
+            cameraText.GetComponent<Text>().text = "1";
+        }
+        for (int i = 0; i < cameraItem.Length; ++i)
+        {
+            if (i == nowCamera)
+            {
+                cameraItem[i].enabled = true;
+                cameraText.GetComponent<Text>().text = (i + 1).ToString();
+            }
+            else
+            {
+                cameraItem[i].enabled = false;
+            }
+        }
     }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;//Scene을 불러 오는 경우 사용
                                   //게임 재시작등
+
 
 public class GameManager : MonoBehaviour
 {
@@ -10,11 +12,25 @@ public class GameManager : MonoBehaviour
 
     public ItemBox[] itemBoxes;
 
+    public Camera[] cameraItem;//카메라 활성 비활성화 
+
+    int cameraCount = 3;//카메라 갯수
+    int nowCamera = 0;// 활성화된 카메라 번호
+
+    public Text cameraText;
+
     public bool isGameWin;
     // Start is called before the first frame update
+
+    public void ClickButton()
+    {
+
+    }
+
     void Start()
     {
-        isGameWin = false;   
+        isGameWin = false;
+        cameraText.GetComponent<Text>().text = "1";//게임 첫 시작시 메인카메라.
     }
 
     // Update is called once per frame
@@ -44,6 +60,29 @@ public class GameManager : MonoBehaviour
             isGameWin = true;
             winUI.SetActive(true);
             // 자기 자신을 보였다 안보였다 할 수 있다!
+        }
+
+        if (Input.GetButtonUp("Fire3"))//카메라 시점 전환 left shit
+        {
+            ++nowCamera;
+
+            if (nowCamera >= cameraCount)
+            {
+                nowCamera = 0;
+                cameraText.GetComponent<Text>().text = "1";
+            }
+            for (int i = 0; i < cameraItem.Length; ++i)
+            {
+                if(i == nowCamera)
+                {
+                    cameraItem[i].enabled = true;
+                    cameraText.GetComponent<Text>().text = (i + 1).ToString();
+                }
+                else
+                {
+                    cameraItem[i].enabled = false;
+                }
+            }
         }
     }
 }
